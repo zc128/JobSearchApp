62d6e652dbd51de5b9d04ba6b92d6cf535e7d819
ZCai <zc128@users.noreply.github.com>
2021-03-26 03:36:32 -0400

    Make the App Installable

---
+++ JobSearchApp/wwwroot/sw.js
@@ -0,0 +1,61 @@
+﻿//const cacheName = 'v1';
+
+function swInstall(event) {
+    // Perform install steps
+    console.info('Service Worker Installing...');
+  //event.waitUntil(
+  //    caches.open('v1').then((cache) => {
+  //        return cache.addAll([
+  //            './',
+  //            './css/site.css',
+  //            './js/site.js',
+  //            './favicon.ico',
+  //            './home/privacy'
+  //        ]);
+  //    })
+  //);
+  //console.info('Finished Caching Website...')
+}
+self.addEventListener('install', swInstall);
+//NetworkOnly
+/*self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request).then(function (networkResponse) {
+            return networkResponse
+        })
+    )
+})*/
+
+//Cache only
+/*self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cacheResponse) {
+            return cacheResponse
+        })
+    )
+})*/
+
+//NewWork First
+/*self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+            .then(function (networkResponse) {
+                return networkResponse
+            }).catch(function () {
+                return caches.match(event.request)
+            })
+    )
+})*/
+
+//Cache Fir
+//self.addEventListener('fetch', function (event)
+//    event.respondWit
+//        caches.match(event.request).then(function (cacheResponse)
+//            if (cacheResponse)
+//                return cacheRespons
+//            } else
+//                return fetch(event.request).then((netResp) => netRes
+//           
+//        
+//   
+//})
